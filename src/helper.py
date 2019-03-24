8fd20aae4e6efb094c3ed82a3552c5b42b25a181
Zijun Chen <c.zijunchen@gmail.com>
2019-03-24 16:41:09 +1100

    Added TwitterReader interface

--- src/helper.py
+++ src/helper.py
@@ -26,3 +26,23 @@ class MelbGrid:
     def grid_idx_to_id(self, idx: int) -> str:
         return self._melb_grids[idx]['id']
 
+
+class TwitterReader:
+    def __init__(self, file_name: str) -> None:
+        self._file = open(file_name, 'r')
+        # ignore the first line (eg. "{"total_rows":3877777,"offset":805584,"rows":[")
+        self._file.readline()
+        self._line_count = -1
+        self._line_skip_num = 1
+        self._line_skip_offset = 0
+    
+    def set_line_skip(self, num: int, offset: int) -> None:
+        self._line_skip_num = num
+        self._line_skip_offset = offset
+
+    def read_one_twitter(self) -> Optional[Tuple[int, List[float], List[str]]]:
+        self._line_count += 1
+        return None
+
+    def __del__(self) -> None:
+        self._file.close()
