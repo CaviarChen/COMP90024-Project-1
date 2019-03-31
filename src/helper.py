e4b5274be95e2b87247242a8109e9d7f95615c7d
Mars Xue <marsxue0221@gmail.com>
2019-03-31 14:25:34 +1100

    Update

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,5 @@
 from typing import *
+from heapq import nlargest
 import json
 
 
@@ -75,8 +76,8 @@ class GridDataCounter:
         self._post_count = 0
         self._tag_dict: Dict[str, int] = {}
 
-    def add_post(self) -> None:
-        self._post_count += 1
+    def add_post(self, num: int = 1) -> None:
+        self._post_count += num
     
     def add_tag(self, tag: str) -> None:
         if tag in self._tag_dict:
@@ -90,3 +91,11 @@ class GridDataCounter:
 
     def marshal_data(self) -> Tuple[int, List[Tuple[str, int]]]:
         return (self._post_count, list(self._tag_dict.items()))
+
+    def get_post_count(self) -> int:
+        return self._post_count
+    
+    def get_tag_dict(self) -> Dict[str, int]:
+        # Heap to get top 5 tags
+        return nlargest(5, self._tag_dict, key=lambda e:e[1])
+
