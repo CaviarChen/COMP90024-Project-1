9c4ca56546522b69b06f4547a628f90444f63839
Zijun Chen <c.zijunchen@gmail.com>
2019-03-31 17:44:20 +1100

    Completed first version (untested)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI
+from typing import *
 import argparse
 import helper
 
@@ -10,14 +11,17 @@ def main() -> None:
 
     args = parser.parse_args()
     melb_grid = helper.MelbGrid(args.melbgrid)
+    grid_num = melb_grid.get_grid_num()
 
-    counters = [helper.GridDataCounter() for _ in range(melb_grid.get_grid_num())]
+    counters: List[Optional[helper.GridDataCounter]] = [helper.GridDataCounter() for _ in range(grid_num)]
 
     mpi_comm = MPI.COMM_WORLD
     node_num = mpi_comm.Get_size()
     node_rank = mpi_comm.Get_rank()
 
+    # ----
     # Stage 1: read file & count data separately
+    # ----
     twitter_reader = helper.TwitterReader(args.twitter, node_num, node_rank)
     while True:
         data = twitter_reader.read_one_twitter()
@@ -31,10 +35,54 @@ def main() -> None:
     del twitter_reader
     # print([c._post_count for c in counters])
 
+    # ----
     # Stage 2: collect & merge data separately (each node will be assgined some grids)
+    # ----
+    # gather data
+    gathered_data: List[Optional[List[Any]]] = [None for _ in range(grid_num)]
+    for i in range(grid_num):
+        # node with rank == assigned_node shoule handled this grid
+        assigned_node = i % node_num
+        gathered_data[i] = mpi_comm.gather(counters[i].marshal_data(), root = assigned_node)
+        if assigned_node != node_rank:
+            # not assigned to this node, free counter
+            counters[i] = None
     
+    # merge data
+    results: List[Any] = [None for _ in range(grid_num)]
+    for i in range(grid_num):
+        if counters[i] is not None:
+            for d in gathered_data[i]:
+                (post_num, tag_list) = d
+                counters[i].add_post(post_num)
+                counters[i].merge_tags(tag_list)
+                # top 5 tags
+                results[i] = counters[i].get_result(5)
 
+    # ----
     # Stage 3: the root node collects the results of different grid and output
+    # ----
+    if node_rank == 0:
+        final_results = [None for _ in range(grid_num)]
+
+    for i in range(grid_num):
+        assigned_node = i % node_num
+        # data on current node, no need to send/recv data
+        if assigned_node == 0 and node_rank == 0:
+            final_results[i] = results[i]
+            continue
+
+        if assigned_node == node_rank:
+            mpi_comm.send(results[i], dest=0, tag=i)
+            continue
+
+        if node_rank == 0:
+            final_results[i] = mpi_comm.recv(source=assigned_node, tag=i)
+            continue
+    del results
+    
+    if node_rank == 0:
+        print(final_results)
 
 if __name__ == "__main__":
     main()
