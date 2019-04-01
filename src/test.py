c17bf037a32f0203e4813ecfa280a1f1d5c6a7b9
Mars Xue <marsxue0221@gmail.com>
2019-04-01 17:15:45 +1100

    Modify test script

--- src/test.py
+++ src/test.py
@@ -1,36 +1,39 @@
 import unittest
-import helper
+from helper import *
 
 class TestStringMethods(unittest.TestCase):
     
     def test_melb_grid(self):
 
-        melb_grid = helper.MelbGrid('dataset/melbGrid.json')
+        melb_grid = MelbGrid('dataset/melbGrid.json')
 
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(144.7, -37.65)),  'A1')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(144.85, -37.65)), 'A1')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.0, -37.65)),  'A2')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.15, -37.65)), 'A3')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.3, -37.65)),  'A4')
+        grid_dict = {
+            (144.70, -37.65): 'A1',
+            (144.85, -37.65): 'A1',
+            (145.00, -37.65): 'A2',
+            (145.15, -37.65): 'A3',
+            (145.30, -37.65): 'A4',
+            (144.70, -37.80): 'B1',
+            (144.85, -37.80): 'B1',
+            (145.00, -37.80): 'B2',
+            (145.15, -37.80): 'B3',
+            (145.30, -37.80): 'B4',
+            (144.70, -37.95): 'C1',
+            (144.85, -37.95): 'C1',
+            (145.00, -37.95): 'C2',
+            (145.15, -37.95): 'C3',
+            (145.30, -37.95): 'C4',
+            (145.00, -38.10): 'D3',
+            (145.15, -38.10): 'D3',
+            (145.30, -38.10): 'D4'
+        }
 
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(144.7, -37.8)),   'B1')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(144.85, -37.8)),  'B1')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.0, -37.8)),   'B2')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.15, -37.8)),  'B3')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.3, -37.8)),   'B4')
-
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(144.7, -37.95)),  'C1')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(144.85, -37.95)), 'C1')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.0, -37.95)),  'C2')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.15, -37.95)), 'C3')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.3, -37.95)),  'C4')
-
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.0, -38.1)),   'D3')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.15, -38.1)),  'D3')
-        self.assertEqual(melb_grid.grid_idx_to_id(melb_grid.find_grid_idx(145.3, -38.1)),   'D4')
+        for i in grid_dict:
+            idx = melb_grid.find_grid_idx(i[0], i[1])
+            self.assertEqual(melb_grid.grid_idx_to_id(idx), grid_dict[i])
 
         print('Assert MelbGrid Done.')
-    
+
 
 if __name__ == '__main__':
     unittest.main()
