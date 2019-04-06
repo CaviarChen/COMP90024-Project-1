9ac38732050662c68e1d46509dcf71ba9e7d2f9f
Mars Xue <marsxue0221@gmail.com>
2019-04-06 14:03:41 +1100

    Add comments

--- src/helper.py
+++ src/helper.py
@@ -6,7 +6,17 @@ import time
 
 
 class MelbGrid:
+    """MelbGrid stores the information about melbourne grid.
+
+    Attributes:
+        _melb_grids: list of dictionaries of each grid with id, longitude and latitude.
+    """
     def __init__(self, file_name: str) -> None:
+        """Initialization. Read the grid file, and store it.
+
+        Args:
+            file_name (str): file name of grid data.
+        """
         with open(file_name, 'r') as file:
             data = json.load(file)
 
@@ -19,24 +29,61 @@ class MelbGrid:
                     'ymin': line['properties']['ymin'],
                     'ymax': line['properties']['ymax']}
             self._melb_grids.append(grid)
-        # sort melb grid by ymin and xmin values
+        # sort grids by latitude descendingly and longitude ascendingly
         self._melb_grids = sorted(self._melb_grids, key=lambda e: (-e['ymin'], e['xmin']))
 
     def find_grid_idx(self, x: float, y: float) -> Optional[int]:
+        """Find the grid index by its coordinate.
+
+        Args:
+            x (float): longitude coordinate.
+            y (float): latitude coordinate.
+
+        Returns:
+            int: grid index, none if not found.
+        """
         for idx, grid in enumerate(self._melb_grids):
             if grid['xmin'] <= x <= grid['xmax'] and grid['ymin'] <= y <= grid['ymax']:
                 return idx
         return None
 
     def grid_idx_to_id(self, idx: int) -> str:
+        """Find the id by the given grid index.
+
+        Args:
+            idx (int): index in grid list.
+
+        Returns:
+            str: corresponding grid id.
+        """
         return self._melb_grids[idx]['id']
-    
+
     def get_grid_num(self) -> int:
+        """Return the number of stored grids.
+
+        Returns:
+            int: the number of grids.
+        """
         return len(self._melb_grids)
 
 
 class TwitterReader:
+    """TwitterReader is a helper class for reading the twitter.
+
+    Attribtues:
+        _file_: file used to read.
+        _line_count: line count of line.
+        _file_start: start position of file.
+        _file_end: end position of file.
+    """
     def __init__(self, file_name: str, node_num: int, node_rank: int) -> None:
+        """Initialization.
+
+        Args:
+            file_name (str): file name of twitter data.
+            node_num (int): the total number of nodes.
+            node_rank (int): the rank of current node.
+        """
         self._file = open(file_name, 'r')
         self._line_count = 0
 
@@ -53,8 +100,16 @@ class TwitterReader:
         self._file_end = self._align_to_newline(rough_end)
         # move to the right pos
         self._file.seek(self._file_start)
-        
+
     def _align_to_newline(self, rough_pos: int) -> int:
+        """Align to avoid the newline or EOF.
+
+        Args:
+            rough_pos(int): rough position for offset.
+
+        Returns:
+            int: optimized position.
+        """
         self._file.seek(rough_pos)
         # keep read util reach a newline or EOF
         while self._file.read(1) not in ['', '\n']:
@@ -62,6 +117,11 @@ class TwitterReader:
         return self._file.tell()
 
     def read_one_twitter(self) -> Optional[Tuple[int, List[float], List[str]]]:
+        """Read one twitter data to extract the data.
+
+        Returns:
+            (int, [float, [str]]): line count and list of coordinate and its hashtag list.
+        """
         line = self._file.readline()
         # stop if reach the last line (eg. ']}') or reachs out of the chunk that assigned to this node
         if self._file.tell() > self._file_end or line.strip() == ']}':
@@ -69,7 +129,7 @@ class TwitterReader:
 
         # parse row
         try:
-            data = json.loads(line.rstrip(",\r\n "))            
+            data = json.loads(line.rstrip(",\r\n "))
             coord = list(map(float, data['doc']['coordinates']['coordinates']))
             # check that coordinates have two values
             assert len(coord) == 2
@@ -84,27 +144,59 @@ class TwitterReader:
         return (self._line_count, coord, hashtags)
 
     def __del__(self) -> None:
+        """Close the file.
+        """
         self._file.close()
 
+
 class GridDataCounter:
+    """GridDataCounter stored the specific results for grid.
+
+    Attributes:
+        _post_count (int): post count for grid.
+        _tag_dict {str: int}: dictionary of tags with its occurrence.
+    """
     def __init__(self) -> None:
+        """Initialization.
+        """
         self._post_count = 0
         self._tag_dict: Dict[str, int] = {}
 
     def add_post(self, num: int = 1) -> None:
+        """Add the given number into post count attribute.
+
+        Args:
+            num (int): default is 1.
+        """
         self._post_count += num
 
     def add_tag(self, tag: str) -> None:
+        """Add the occurrence of the given tag.
+
+        Args:
+            tag (str): tag.
+        """
         if tag in self._tag_dict:
             self._tag_dict[tag] += 1
         else:
             self._tag_dict[tag] = 1
 
     def add_tags(self, tags: List[str]) -> None:
+        """Add the occurrence of all the present tags.
+
+        Args:
+            tags ([str]): list of tags.
+        """
         for t in tags:
             self.add_tag(t)
 
     def merge_tags(self, tags: List[Tuple[str, int]]) -> None:
+        """Merge all the present tags into stored tag dictionary.
+
+        Args:
+            tags ([(str, int)]): list of tag and its count.
+
+        """
         for tag in tags:
             if tag[0] in self._tag_dict:
                 self._tag_dict[tag[0]] += tag[1]
@@ -112,28 +204,55 @@ class GridDataCounter:
                 self._tag_dict[tag[0]] = tag[1]
 
     def marshal_data(self) -> Tuple[int, List[Tuple[str, int]]]:
+        """Marshal the data into optimized type.
+
+        Returns:
+            (int, [(str, int)]): post count and results of hashtag and count.
+        """
         return (self._post_count, list(self._tag_dict.items()))
 
     def get_result(self, n: int) -> Tuple[int, List[Tuple[str, int]]]:
+        """Get the top n results by distinct values.
 
+        Args:
+            n (int): top count.
+
+        Returns:
+            (int, [(str, int)]): post count and top n results of hashtag and count.
+        """
         topk_freq = heapq.nlargest(n, set(self._tag_dict.values()))
         if len(topk_freq) < n:
-            # not enought tags, take everything
+            # No enought tags, take everything
             tags = list(self._tag_dict.items())
         else:
             tags = list(filter(lambda x: x[1] >= topk_freq[-1], self._tag_dict.items()))
 
         return (self._post_count, sorted(tags, key=lambda x: -x[1]))
 
+
 class Timer:
+    """Timer is used to count the stage time.
+
+    Attributes:
+        _stage_time ([(float, str)]): stored stage time with each given string.
+    """
     def __init__(self) -> None:
+        """Initialization, add the start time in advance.
+        """
         self._stage_time: List[Tuple[float, str]] = []
         self.add_stage_time("")
 
     def add_stage_time(self, name: str) -> None:
+        """Add one stage time period.
+
+        Args:
+            name (str): specific name for this time period.
+        """
         self._stage_time.append((time.time(), name))
 
     def print_result(self) -> None:
+        """Print the stage time results in specific format.
+        """
         total_time: float = 0
         for i in range(1, len(self._stage_time)):
             time_used = self._stage_time[i][0] - self._stage_time[i-1][0]
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from mpi4py import MPI
 from typing import *
-import datetime
 import argparse
+import datetime
 import helper
 
 
 def main() -> None:
+    """Main function is used to execute the task.
+    """
     # Parser usage
     parser = argparse.ArgumentParser(usage='mpiexec -n 1 python src/main.py dataset/melbGrid.json dataset/tinyTwitter.json')
     parser.add_argument('melbgrid', type=str)
@@ -18,13 +20,30 @@ def main() -> None:
 
 
 class Task:
+    """Task class is used to run all the stages for given dataset.
+
+    Attributes:
+        _grid_file (str): file of grid data.
+        _twitter_file (str): file of twitter data.
+        _mpi_comm: MPI.COMM_WORLD.
+        _node_num: the total number of nodes.
+        _node_rank: the rank of current node.
+    """
     def __init__(self, grid_file: str, twitter_file: str, mpi_comm: MPI.Intracomm) -> None:
+        """Initialization.
+
+        Args:
+            grid_file (str): file of grid data.
+            twitter_file (str): file of twitter data.
+            mpi_comm (MPI.Intracomm): MPI.COMM_WORLD.
+        """
         self._grid_file = grid_file
         self._twitter_file = twitter_file
         self._mpi_comm = mpi_comm
         self._node_num = mpi_comm.Get_size()
         self._node_rank = mpi_comm.Get_rank()
 
+        # root node
         if self._node_rank == 0:
             self._print_title("Task")
             print("task_time:   ", datetime.datetime.now())
@@ -34,6 +53,8 @@ class Task:
             print()
 
     def execute(self) -> None:
+        """Execute the task.
+        """
         # Start timer
         if self._node_rank == 0:
             timer = helper.Timer()
@@ -63,13 +84,15 @@ class Task:
             self._print_title("Timer")
             timer.print_result()
 
-
     def _stage1(self) -> Tuple[helper.MelbGrid, List[helper.GridDataCounter]]:
         """
         Stage 1: load & count data
         - Load the melbourne grid file.
         - Read a part of the twitter file & count posts and hashtags.
         - No communication between nodes in this stage.
+
+        Returns:
+            [GridDataCounter]: list of GridDataCounter for all grids.
         """
         melb_grid = helper.MelbGrid(self._grid_file)
         grid_num = melb_grid.get_grid_num()
@@ -84,26 +107,33 @@ class Task:
             (_, coord, hashtags) = data
             # skip when the data is not right
             if len(coord) != 2:
-                continue 
+                continue
             idx = melb_grid.find_grid_idx(coord[0], coord[1])
             if idx is not None:
                 counters[idx].add_post()
                 counters[idx].add_tags(hashtags)
         del twitter_reader
-        
+
         return (melb_grid, counters)
 
     def _stage2a(self, counters: List[Optional[helper.GridDataCounter]], grid_num: int) -> List[Optional[List[Any]]]:
         """
         Stage 2a: collect data of each grid
-        - Grid with id i will be handled on node (i % node_num), 
+        - Grid with id i will be handled on node (i % node_num),
         so that node needs to collect data of grid i from all other nodes.
+
+        Args:
+            counters ([GridDataCounter]): list of GridDataCounter for all grids.
+            grid_num (int): the number of grids.
+
+        Returns:
+            [[(str, int)]]: list of all executed data for all grids.
         """
         gathered_data: List[Optional[List[Any]]] = [None for _ in range(grid_num)]
         for i in range(grid_num):
             # node with rank == assigned_node shoule handled this grid
             assigned_node = i % self._node_num
-            gathered_data[i] = self._mpi_comm.gather(counters[i].marshal_data(), root = assigned_node)
+            gathered_data[i] = self._mpi_comm.gather(counters[i].marshal_data(), root=assigned_node)
             if assigned_node != self._node_rank:
                 # not assigned to this node, free counter
                 counters[i] = None
@@ -114,6 +144,13 @@ class Task:
         Stage 2b: merge data of each grid
         - Grid with id i will be handled on node (i % node_num).
         - No communication between nodes in this stage.
+
+        Args:
+            gathered_data ([[(str, int)]]): list of all executed data for all grids.
+            grid_num (int): the number of grids.
+
+        Returns:
+            [(int, [(str, int)])]: list of tuple of post count and list of its hashtag and count.
         """
         results: List[Any] = [None for _ in range(grid_num)]
         for i in range(grid_num):
@@ -131,6 +168,10 @@ class Task:
         """
         Stage 3: final output
         - The root node collects the results from all other nodes and output.
+
+        Args:
+            melb_grid (MelbGrid): MelbGrid class.
+            results ([(int, [(str, int)])]): list of tuple of post count and list of its hashtag and count.
         """
         grid_num = melb_grid.get_grid_num()
         if self._node_rank == 0:
@@ -143,15 +184,18 @@ class Task:
                 final_results[i] = results[i]
                 continue
 
+            # nodes except the root node will send data to root node.
             if assigned_node == self._node_rank:
                 self._mpi_comm.send(results[i], dest=0, tag=i)
                 continue
 
+            # root node will receive all the data.
             if self._node_rank == 0:
                 final_results[i] = self._mpi_comm.recv(source=assigned_node, tag=i)
                 continue
         del results
 
+        # root node will be responsible for the final output.
         if self._node_rank == 0:
             post_list = []
             hashtag_list = []
@@ -165,11 +209,12 @@ class Task:
             self._print_title("Result")
             for post_i in post_list:
                 print("{}: {} posts {}".format(melb_grid.grid_idx_to_id(post_i[0]), post_i[1], tuple(hashtag_list[post_i[0]])))
-    
+
     def _print_title(self, title: str) -> None:
         print()
         print(' ', title)
         print('-'*20)
+
 
 if __name__ == "__main__":
     main()
